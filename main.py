859dae33c117276b8f4fba9c2fcf6628b99772b8
박찬솔 <chansolpark7@naver.com>
2025-06-24 09:53:56 +0900

    파일 입력

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 import json
+from collections import defaultdict
 
-for i in range(10):
-    print(i+1)
+def read_map():
+    with open('Data_Set.json', 'rt', encoding='utf-8') as file:
+        raw_data = json.load(file)
+
+    depot = raw_data['depot']['location']
+    destinations = dict()
+    for destination in raw_data['destinations']:
+        destinations[destination['destination_id']] = destination['location']
+
+    return depot, destinations
+
+def read_OD_matrix():
+    with open('distance-data.txt', 'rt', encoding='utf-8') as file:
+        file.readline()
+        OD_matrix = defaultdict(dict)
+        while True:
+            string = file.readline().rstrip()
+            if not string: break
+            origin, destination, time_min, distance_meter = string.split()
+            time_min = float(time_min)
+            distance_meter = int(distance_meter)
+            OD_matrix[origin][destination] = (time_min, distance_meter)
+
+    return OD_matrix
+
+def main():
+    depot, destinations = read_map()
+    OD_matrix = read_OD_matrix()
+    
+    print(depot)
+    print(destinations['D_00001'])
+    print(OD_matrix['D_00001']['Depot'])
+
+if __name__ == '__main__':
+    main()
---
+++ visualize.py
@@ -0,0 +1,24 @@
+import main
+import pygame
+
+# main.function()
+
+# 여기다 시각화
+
+# main의 어떤 함수를 이용해서 depo, destinations 정보 불러오는 구조
+# main.function() -> [depo, destinations]
+# - depo = {'longtitude': ~, 'latitude': ~}
+# - destinations[destination_id] = {'longtitude': ~, 'latitude': ~}
+
+def show():
+    size = (600, 600)
+
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+
+    clock = pygame.time.Clock()
+
+    depot, destinations = main.read_map()
+
+    while True:
+        clock.tick(60)
